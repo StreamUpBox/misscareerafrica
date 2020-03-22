08246fb05f05079d9f9b9417873ac2ef1c2402b7
Ganza respice <respinho2014@gmail.com>
2020-03-22 20:47:36 +0200

    ...

--- resources/views/eligibility.blade.php
+++ resources/views/eligibility.blade.php
@@ -31,27 +31,30 @@
                 <div class="container" id="contact" style="background:black;">
                     <p style="color: black;">
                         <br><a href="/candidate-application">APPLY HERE</a><br>
+
                         <b style="font-size:20px">Eligible candidate(s):</b><br />
-                        -Young female professionals<br />
-                        -Entrepreneurs High School graduates, University Students and working class(Under 24
-                        years).<br />
-                        -Southern Africa and<br />
+                        -Young female professionals
+                        -Entrepreneurs
+                        -High School graduates & University Students. -Working class(Under 24 years)
                         -In a range of 18-24 Years
-                    </p>
+                        -Speaking either English or French
+                        -Viable Business Plan or Idea
+
+                        Application Requirements:
+                        -Complete Online Application as required!
+                        -Incomplete Applications will not be accepted.
+                        What else is required?
+                        Online Voting & Boot Camp Tickets orientation is required and the information regarding
+                        that session will be given to you 3 weeks before the boot camp. Transportation Affairs: Note:
+                        Miss Career Africa Organization/The competition is not responsible of the candidate's transports
+                        to the country that is hosting both Boot camp and Grand Finale.
+                        Therefore, one should be optimistic to raise her own transport budget from friends, families,
+                        school and other interested organizations. One should cater for her dress-codes during the boot
+                        camp and Grand finale. Ps: The competition only provides accommodations, meals, airport pick-up
+                        and internal transports for those attending grand finale:)
+                      
                     <p>
-                        <a href="#">Application Requirements:</a><br>
-                        Complete Online Application as required!<br />
-                        Ps: Incomplete Applications will not be accepted.<br>
-                        <a href="#">What else is required?</a><br>
-                        Boot Camp orientation is required and the information regarding <br /> that session will be given to you 3 weeks before the boot camp.
-                        <!-- If you are lucky to be nominated for the boot camp, Its an eye-Opener program you do not want to
-                        miss. your availability for the Boot Camp orientation for all the Miss Career Africa sessions,
-                        taking place November , 6th 2019 is required. <br />
-                        Note: If you cannot make it on that day , call +250784872667 before the meeting date.
-                        <br />Attend the Information Session:<br />
-                        The Boot Camp information session will be held in Kigali. <br />For Further Information, Emails
-                        will be sent to you.
-                        <br> -->
+                      
                         <a href="#">Application timeline:</a><br>
                         Application open: Sunday 22nd Mar,2020<br>
                         Deadline: Starday, 18th Apr,2020.(11:59) PM.
