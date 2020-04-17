ccd93c08ef19110e49b1cbd47007ddd9a618b6b5
Umujyi.com <umujyi2019@gmail.com>
2020-04-18 01:11:51 +0200

    ..

--- public/js/mca.js
+++ public/js/mca.js
@@ -70,6 +70,30 @@ function readMore(name,src,bio){
     $(".profile").attr("src", src);
 }
 
+function compareValues(key, order = 'desc') {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
+  
+      const varA = (typeof a[key] === 'string')
+        ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string')
+        ? b[key].toUpperCase() : b[key];
+  
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
+
 function loadSelectedCandidates(){
     $('.loading-overlay').show();
     var htmlss= $('#selected_candidates');
@@ -88,6 +112,7 @@ function loadSelectedCandidates(){
           //  console.log(response);
             if(response.success && response.data.length > 0){
             const datas=response.data;
+            datas.sort(compareValues('votes'));
             datas.forEach(element => {
                 if(element){
                    
@@ -143,7 +168,7 @@ function loadSelectedCandidates(){
 
 
 function votes(id,votes){
-    
+
 return alert('Voting system is closed');
 
 return window.location.href="candiateVoters/"+id;
