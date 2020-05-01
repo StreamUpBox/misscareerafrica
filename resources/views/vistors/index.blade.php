9ecdd3530556c62c320eb771f3959a16afcf9d3d
Umujyi.com <umujyi2019@gmail.com>
2020-05-01 14:18:13 +0200

    ..

--- resources/views/vistors/index.blade.php
+++ resources/views/vistors/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <?php $visitorss=\App\Models\Vistors::where('id','!=',0)->count(); ?>
     <section class="content-header">
-        <h1 class="pull-left">{{count($visitorss)}} Vistor(s)</h1>
+        <h1 class="pull-left">{{$visitorss}} Vistor(s)</h1>
         <h1 class="pull-right">
            <!-- <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('sessions.create') !!}">Add New</a> -->
         </h1>
